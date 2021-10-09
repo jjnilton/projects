97bfa8739e5adc3d556ed6f71a64bd89ceb1a47d
JJ <jjnilton@users.noreply.github.com>
2021-10-09 19:46:05 -0300

    sorting api output, handling query params

--- word-frequency/pages/api/[text].js
+++ word-frequency/pages/api/[text].js
@@ -1,9 +1,39 @@
-import calculateOccurrences from "../../utils/calculateOccurrences"
+import calculateOccurrences from "../../utils/calculateOccurrences";
 
 export default function handler(req, res) {
+  //five five five five five one two two three three three four four four four
 
-  const occurrences = calculateOccurrences(req.query.text)
-  console.log(occurrences)
+  const occurrences = calculateOccurrences(req.query.text);
 
-  res.status(200).json({data: occurrences })
+  const sort = {
+    type: req.query.sort || "number",
+    ascending: req.query.ascending || "false",
+  };
+
+  const sortedOccurrences = (input, type, ascending) => {
+    console.log(type, ascending);
+    let sorted = input;
+    if (type === "word" && ascending === "false") {
+      sorted = Object.entries(occurrences).sort((a, b) =>
+        a[0] > b[0] ? -1 : 1
+      );
+    } else if (type === "word") {
+      sorted = Object.entries(occurrences).sort((a, b) =>
+        a[0] < b[0] ? -1 : 1
+      );
+    } else if (type === "number" && ascending === "true") {
+      sorted = Object.entries(occurrences).sort((a, b) =>
+        a[1] < b[1] ? -1 : 1
+      );
+    } else {
+      sorted = Object.entries(occurrences).sort((a, b) =>
+        a[1] > b[1] ? -1 : 1
+      );
+    }
+    return Object.fromEntries(sorted);
+  };
+
+  const output = sortedOccurrences(req.query.text, sort.type, sort.ascending);
+
+  res.status(200).json({ data: output });
 }
