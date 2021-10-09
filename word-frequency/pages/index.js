c1d6501d7dd9eb5409a71a57789a3f2d9b0cad0a
JJ <jjnilton@users.noreply.github.com>
2021-10-09 11:34:02 -0300

    add react-chartjs

--- word-frequency/pages/index.js
+++ word-frequency/pages/index.js
@@ -1,29 +1,61 @@
 import styled from "styled-components";
+import { Pie } from "react-chartjs-2";
+import { useState } from "react";
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `;
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-
-  const formData = new FormData(event.target);
-  const text = formData.get("text");
-  const words = text.split(" ");
-
-  console.log(words)
-
-  const occurrences = words.reduce((acc, curr) => {
-    return acc[curr] = acc[curr] + 1 || 1, acc
-  }, {})
-
-  console.log(occurrences)
-
-}
-
-
 const Home = () => {
+  const [data, setData] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const text = formData.get("text").trim();
+    const words = text.split(" ");
+
+    console.log(words);
+
+    const occurrences = words.reduce((acc, curr) => {
+      return (acc[curr] = acc[curr] + 1 || 1), acc;
+    }, {});
+
+    console.log(occurrences);
+
+    const labels = Object.keys(occurrences);
+    const values = Object.values(occurrences);
+
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          data: values,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+
+    setData(data);
+  };
 
   return (
     <>
@@ -33,6 +65,7 @@ const Home = () => {
         <button>Calculate</button>
       </form>
       <div>Result: </div>
+      <Pie data={data} height={100} width={50}></Pie>
     </>
   );
 };
