9bce54b9b5433ea8d624bcbf18ecdf5b38921bc7
JJ <jjnilton@users.noreply.github.com>
2021-10-22 10:25:28 -0300

    code cleanup

--- book-finder-app/src/App.test.js
+++ book-finder-app/src/App.test.js
@@ -2,26 +2,37 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
-import userEvent from '@testing-library/user-event';
+import userEvent from "@testing-library/user-event";
 
 test("renders the search component", () => {
-  render(<Provider store={store}><App /></Provider>);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
   const searchElement = screen.getByPlaceholderText("Search...");
   expect(searchElement).toBeInTheDocument();
 });
 
 test("search input works as intended", () => {
-  render(<Provider store={store}><App /></Provider>);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
   const searchElement = screen.getByPlaceholderText("Search...");
-  userEvent.type(searchElement, "nine princes in amber")
-  expect(searchElement).toHaveValue('nine princes in amber');
+  userEvent.type(searchElement, "nine princes in amber");
+  expect(searchElement).toHaveValue("nine princes in amber");
 });
 
-
 test("disables button while loading", () => {
-  render(<Provider store={store}><App /></Provider>);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
   const searchButton = screen.getByText("Search");
-  const searchElement = screen.getByPlaceholderText("Search...")
+  const searchElement = screen.getByPlaceholderText("Search...");
   userEvent.click(searchButton);
   expect(searchElement).toBeDisabled();
 });
