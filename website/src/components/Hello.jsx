3fa2fde8979b6984d509ac00ed5049c5c021eec6
JJ <jjnilton@users.noreply.github.com>
2021-10-25 18:22:21 -0300

    add animation to hello

--- website/src/components/Hello.jsx
+++ website/src/components/Hello.jsx
@@ -1,26 +1,37 @@
 import { useContext } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import Context from "../store/context";
+
+const slideUp = keyframes`
+  from {
+    transform: translateY(16%);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0%);
+    opacity: 1;
+  }
+`;
 
 const content = {
   firstHalf: {
     en: "Hello, I'm NAME, and",
-    pt: "Olá, eu sou o NOME e"
+    pt: "Olá, eu sou o NOME e",
   },
   secondHalf: {
     en: "this is my website.",
-    pt: "este é o meu website."
-  }
+    pt: "este é o meu website.",
+  },
 };
 
 const StyledHello = styled.section`
   font-size: 2em;
   text-align: center;
   color: ${({ theme }) => theme.colors.secondary};
+  animation: ${slideUp} 2s;
   h1:first-child {
     font-size: 1em;
     margin-bottom: 0;
-
   }
   h1:last-child {
     margin-top: 0;
