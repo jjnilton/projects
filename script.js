2eb899c18b9c311edd63d9b71279da7b6bf5a7b0
JJ <jjnilton@users.noreply.github.com>
2021-11-01 09:22:23 -0300

    fix reference to removed element, remove mix-blend-mode

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 window.addEventListener("scroll", (event) => {
   if (window.scrollY > 0) {
     document.querySelector("header").classList.add("scrolled");
-    document.querySelector(".header-bg").setAttribute("hidden", "true");
+    // document.querySelector(".header-bg").setAttribute("hidden", "true");
     document.querySelector("header > div.header-wrapper").classList.add("compact");
   } else {
     if (document.querySelector("header").classList.contains("scrolled")) {
       document.querySelector("header").classList.remove("scrolled");
-      document.querySelector(".header-bg").removeAttribute("hidden");
+      // document.querySelector(".header-bg").removeAttribute("hidden");
       document.querySelector("header > div.header-wrapper").classList.remove("compact");
     }
   }
 });
 
 document.querySelector(".menu-button").addEventListener("click", (event) => {
-  console.log("ai ia");
   if (document.querySelector("nav").classList.contains("visible")) {
     document.querySelector("nav").classList.remove("visible");
     document.querySelector(".menu-button").classList.remove("active");
