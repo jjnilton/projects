dcfa34d1623ef08defd80db84928f21fc52c8697
JJ <jjnilton@users.noreply.github.com>
2022-01-08 10:32:16 -0300

    improve message when list is empty

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -195,7 +195,7 @@ const ToDoList = ({
           <Fade in timeout={1000}>
             <Typography color="text.primary" style={{ textAlign: 'center', marginTop: '1em' }}>
               {search.length > 0 || filter !== 'all'
-                ? 'Sorry, no To-Dos found.'
+                ? `Sorry, no ${filter} To-Dos found.`
                 : 'Your To-Do list is empty. Try adding a new To-Do.'}
             </Typography>
           </Fade>
